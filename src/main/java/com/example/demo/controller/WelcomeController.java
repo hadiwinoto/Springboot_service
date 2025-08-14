dafa9bf7905941a2ed83a5111fa95c27ca526803
Hadi Winoto <mulyo.hadi@gunanusa.co.id>
2025-08-14 10:59:38 +0700

    update

--- src/main/java/com/example/demo/controller/WelcomeController.java
+++ src/main/java/com/example/demo/controller/WelcomeController.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringBootVersion;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.HashMap;
 
 @RestController
